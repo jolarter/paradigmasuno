672dbc22c620570fd2898453384c7aaf928e469e
Julian Olarte <jolarter@poligran.edu.co>
2017-10-24 20:02:49 -0500

    Creación de proyecto

---
+++ src/edu/poli/prapr/ejemploversiones/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.poli.prapr.ejemploversiones;
+
+/**
+ * @author julian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo cruel");
+		
+	}
+
+}
